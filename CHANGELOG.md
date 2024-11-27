86f212863937c9d1dfb3226f7b6692fa7dd51931
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2024-11-27 16:23:30 +0100

    JEAP-5071 Update to spring boot 3.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,12 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres
 to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [8.11.0] - 2024-12-18
+
+### Changed
+
+- upgrade spring boot to version 3.4.0
+
 ## [8.10.1] - 2024-12-17
 
 ### Added
 
 - credential scan (trufflehog)
-
 
 ## [8.10.0] - 2024-12-09
 
--- jeap-messaging-infrastructure-kafka-reactive/src/main/java/ch/admin/bit/jeap/messaging/kafka/reactive/JeapSenderReceiverOptionsFactory.java
+++ jeap-messaging-infrastructure-kafka-reactive/src/main/java/ch/admin/bit/jeap/messaging/kafka/reactive/JeapSenderReceiverOptionsFactory.java
@@ -5,6 +5,8 @@ import ch.admin.bit.jeap.messaging.kafka.serde.KafkaAvroSerdeProvider;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.serialization.Serializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.DefaultSslBundleRegistry;
+import org.springframework.boot.ssl.SslBundles;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.SenderOptions;
 
@@ -21,7 +23,9 @@ public class JeapSenderReceiverOptionsFactory {
     final KafkaAvroSerdeProvider kafkaAvroSerdeProvider;
 
     public <K,V> SenderOptions<K,V> createSenderOptions() {
-        Map<String, Object> producerConfig = new HashMap<>(springKafkaProperties.buildProducerProperties());
+        // ssl properties are currently configured directly by jEAP messaging
+        SslBundles sslBundles = new DefaultSslBundleRegistry();
+        Map<String, Object> producerConfig = new HashMap<>(springKafkaProperties.buildProducerProperties(sslBundles));
         producerConfig.putAll(jeapKafkaConfiguration.producerConfig(clusterName));
         SenderOptions<K,V> senderOptions = SenderOptions.create(producerConfig);
         //noinspection unchecked
@@ -31,7 +35,9 @@ public class JeapSenderReceiverOptionsFactory {
     }
 
     public <K,V> ReceiverOptions<K,V> createReceiverOptions() {
-        Map<String, Object> consumerConfig = new HashMap<>(springKafkaProperties.buildConsumerProperties());
+        // ssl properties are currently configured directly by jEAP messaging
+        SslBundles sslBundles = new DefaultSslBundleRegistry();
+        Map<String, Object> consumerConfig = new HashMap<>(springKafkaProperties.buildConsumerProperties(sslBundles));
         consumerConfig.putAll(jeapKafkaConfiguration.consumerConfig(clusterName));
         //noinspection unchecked
         return ReceiverOptions.create(consumerConfig);
