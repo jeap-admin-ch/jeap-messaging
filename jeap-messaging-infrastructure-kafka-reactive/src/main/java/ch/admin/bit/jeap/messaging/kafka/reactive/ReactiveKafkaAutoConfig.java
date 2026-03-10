a4c3180c81c6f7b9b26f00607538946e27d494ea
Mike Rothenbühler <mike.rothenbuehler@bit.admin.ch>
2026-03-10 15:30:54 +0100

    JEAP-6542: remove reactive

--- jeap-messaging-infrastructure-kafka-reactive/src/main/java/ch/admin/bit/jeap/messaging/kafka/reactive/ReactiveKafkaAutoConfig.java
+++
@@ -1,9 +0,0 @@
-package ch.admin.bit.jeap.messaging.kafka.reactive;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Import;
-
-@AutoConfiguration
-@Import(JeapReactiveKafkaBeanRegistrar.class)
-public class ReactiveKafkaAutoConfig {
-}
